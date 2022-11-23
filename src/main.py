2cfd05fdc60cc8429c862ccdf452a4039a4a43d1
Jakub Tolsciuk <jakubtolsciuk@gmail.com>
2022-11-23 20:49:14 +0100

    add shapiro

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
 from scipy import stats
+from scipy.stats import wilcoxon, shapiro
 
 file_path_heart = "./datasets/heart.dat"
 
@@ -46,19 +47,36 @@ def print_stat_from_description(np_arr_stat, stat_name):
 # print(dataset)
 
 # * Mean
-print_stat_from_description(description.mean, stat_name="Mean")
+# print_stat_from_description(description.mean, stat_name="Mean")
 
-# * Variance
-print_stat_from_description(description.variance, stat_name="Variance")
+# # * Variance
+# print_stat_from_description(description.variance, stat_name="Variance")
 
-# * Standard deviation
-std = np.std(dataset)
-print_stat_from_description(pd.Series.to_numpy(std), stat_name="Standard deviation")
+# # * Standard deviation
+# std = np.std(dataset)
+# print_stat_from_description(pd.Series.to_numpy(std), stat_name="Standard deviation")
 
-# # * Skewness
-print_stat_from_description(description.skewness, stat_name="Skewness")
+# # # * Skewness
+# print_stat_from_description(description.skewness, stat_name="Skewness")
 
-# * Kurtosis
-print_stat_from_description(description.kurtosis, stat_name="Kurtosis")
+# # * Kurtosis
+# print_stat_from_description(description.kurtosis, stat_name="Kurtosis")
 
+print(dataset)
+print(type(dataset))
+
+# stat, p = wilcoxon(age, sex)
+# print('Statistics=%.3f, p=%.3f' % (stat, p))
+
+class_1_df = dataset[dataset['Class'] == 1]
+class_2_df = dataset[dataset['Class'] == 2]
+
+rest_blood_pressure_c1 = class_1_df["RestBloodPressure"].to_list()
+rest_blood_pressure_c2 = class_2_df["RestBloodPressure"].to_list()
+
+shapiro_test_blood_pressure_c1 = stats.shapiro(rest_blood_pressure_c1)
+shapiro_test_blood_pressure_c2 = stats.shapiro(rest_blood_pressure_c2)
+
+print(shapiro_test_blood_pressure_c1)
+print(shapiro_test_blood_pressure_c2)
 
