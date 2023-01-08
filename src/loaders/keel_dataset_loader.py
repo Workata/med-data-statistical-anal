c6ffc3b12f3b65dad639beaba8d5595cdfab8522
Jakub Tolsciuk <jakubtolsciuk@gmail.com>
2023-01-08 15:36:16 +0100

    Add freidmanschi test

---
+++ src/loaders/keel_dataset_loader.py
@@ -0,0 +1,57 @@
+import pandas as pd
+from pandas import DataFrame
+from numpy import ndarray
+import re
+from typing import List, Union
+
+
+class KeelDatasetLoader:
+    """
+    FORMAT:
+    @...
+    @...
+    @inputs (features)
+    @outputs (classes)
+    @data
+    (csv data)
+    """
+
+    DATASET_FOLDER = "./datasets"
+
+    def load(self, dataset_name: str, convert_to_ndarray=False, map_classes=False) -> Union[DataFrame, ndarray]:
+        """
+        Returns dataset by given dataset name.
+        By default it's a dataframe, but can be converted to numpy's ndarray.
+        """
+        dataset_path = self._get_dataset_path(dataset_name)
+        dataset_file = self._read_file(file_path=dataset_path)
+        headers = self._get_dataset_headers(dataset_file)
+        dataset_df = pd.read_csv(
+            dataset_path,
+            skiprows=lambda idx : dataset_file.split('\n')[idx].startswith("@"),
+            names=headers,
+            index_col=None
+        )
+        if map_classes:
+            dataset_df = self._map_classes(dataset_df)
+        return dataset_df.to_numpy() if convert_to_ndarray else dataset_df
+
+    def _map_classes(self, df: DataFrame) -> DataFrame:
+        class_mapping = {'negative': 0, 'positive': 1}
+        df['Class'] = df['Class'].apply(lambda x: class_mapping[x.strip()])
+        return df
+
+    def _get_dataset_headers(self, dataset_file: str) -> List[str]:
+        features_str = self._extract_attribute(pattern='@inputs (.*)', content=dataset_file)
+        classes_str = self._extract_attribute(pattern='@outputs (.*)', content=dataset_file)
+        return [*features_str.split(', '), *classes_str.split(', ')]
+
+    def _extract_attribute(self, pattern: str, content: str) -> str:
+        return re.search(pattern , content, re.IGNORECASE).group(1)
+
+    def _get_dataset_path(self, dataset_name: str) -> str:
+        return f"{self.DATASET_FOLDER}/{dataset_name}"
+
+    def _read_file(self, file_path: str):
+        with open(file_path) as f:
+            return f.read()
--- src/main.py
+++ src/main.py
@@ -1,70 +1,57 @@
 import numpy as np
 import pandas as pd
-from scipy import stats
-from scipy.stats import wilcoxon, shapiro
+import matplotlib.pyplot as plt
 
-file_path_heart = "./datasets/heart.dat"
-file_path_thyroid = "./datasets/thyroid.csv"
-file_path_survey = "./datasets/survey.csv"
+from scipy import stats
+from scipy.stats import wilcoxon, shapiro, friedmanchisquare
+from loaders import KeelDatasetLoader
+import scikit_posthocs as sp    #
+
+
 
 np.set_printoptions(suppress=True)
 
-headers_heart = [
-    "Age",
-    "Sex",
-    "ChestPainType",
-    "RestBloodPressure",
-    "SerumCholestoral",
-    "FastingBloodSugar",
-    "ResElectrocardiographic",
-    "MaxHeartRate",
-    "ExerciseInduced",
-    "Oldpeak",
-    "Slope",
-    "MajorVessels",
-    "Thal",
-    "Class",
-]
+dataset_loader = KeelDatasetLoader()
 
-headers_thyroid = [
-    "Age",
-    "Sex",
-    "On_thyroxine",
-    "Query_on_thyroxine",
-    "On_antithyroid_medication",
-    "Sick",
-    "Pregnant",
-    "Thyroid_surgery",
-    "I131_treatment",
-    "Query_hypothyroid",
-    "Query_hyperthyroid",
-    "Lithium",
-    "Goitre",
-    "Tumor",
-    "Hypopituitary",
-    "Psych",
-    "TSH",
-    "T3",
-    "TT4",
-    "T4U",
-    "FTI",
-    "Class"
-]
+theroid_dataset = dataset_loader.load('thyroid.dat')
+print(theroid_dataset)
 
-headers_survey = [
-    "Gender",
-    "Age",
-    "KmBefore",
-    "KgBefore",
-    "TimeBefore",
-    "Medicine1",
-    "Medicine2",
-    "Medicine3",
-    "KmAfter",
-    "KgAfter",
-    "TimeAfter",
-    "SideEffects"
-]
+sample_1 = theroid_dataset.sample(n=100)
+
+
+# * freidmanschi test
+tsh_s1 = sample_1["TSH"].to_list()
+t3_s1 = sample_1["T3"].to_list()
+tt4_s1 = sample_1["TT4"].to_list()
+# tsh_c2 = sample_1[attribute].to_list()
+
+shapiro_test_tsh_s1 = stats.shapiro(tsh_s1)
+shapiro_test_t3_s1 = stats.shapiro(t3_s1)
+shapiro_test_tt4_s1 = stats.shapiro(tt4_s1)
+# shapiro_test_tsh_c2 = stats.shapiro(tsh_c2)
+
+print(shapiro_test_tsh_s1)
+print(shapiro_test_t3_s1)
+print(shapiro_test_tt4_s1)
+# print(shapiro_test_tsh_c2)
+
+freidman_outcome = friedmanchisquare(tsh_s1, t3_s1, tt4_s1)
+print(freidman_outcome)
+
+# sp.posthoc_conover(theroid_dataset, val_col='Sepal.Width', group_col='Species', p_adjust = 'holm')
+pc = sp.posthoc_conover(theroid_dataset, val_col='TSH', group_col='Class')
+heatmap_args = {'linewidths': 0.25, 'linecolor': '0.5', 'clip_on': False, 'square': True, 'cbar_ax_bbox': [0.80, 0.35, 0.04, 0.3]}
+plot = sp.sign_plot(pc, **heatmap_args)
+print(plot)
+print(type(plot))
+plt.savefig('foo.png')
+
+
+# print(samples)
+
+# * Test Friedmana rank
+# freidman_outcome = friedmanchisquare(*samples)
+# print(freidman_outcome)
 
 # dataset = pd.read_csv(
 #     file_path_heart,
@@ -79,12 +66,12 @@ headers_survey = [
 #     index_col=None,
 # )
 
-dataset = pd.read_csv(
-    file_path_survey,
-    names=headers_survey,
-    index_col=None,
-    delimiter=';'
-)
+# dataset = pd.read_csv(
+#     file_path_survey,
+#     names=headers_survey,
+#     index_col=None,
+#     delimiter=';'
+# )
 
 # description = stats.describe(dataset)
 # print(description)
@@ -116,7 +103,7 @@ dataset = pd.read_csv(
 # # * Kurtosis
 # print_stat_from_description(description.kurtosis, stat_name="Kurtosis")
 
-print(dataset)
+# print(dataset)
 # print(type(dataset))
 
 # stat, p = wilcoxon(age, sex)
@@ -164,24 +151,24 @@ print(dataset)
 
 # ! ------------------------- TEST shapiro + Paired t-test  -----------------------------
 
-class_1_df = dataset[dataset['Gender'] == 'M']
-class_2_df = dataset[dataset['Gender'] == 'F']
+# class_1_df = dataset[dataset['Gender'] == 'M']
+# class_2_df = dataset[dataset['Gender'] == 'F']
 
-attribute = "KmAfter"
-attr_list_c1 = class_1_df[attribute].to_list()
-print(np.var(attr_list_c1))
+# attribute = "KmAfter"
+# attr_list_c1 = class_1_df[attribute].to_list()
+# print(np.var(attr_list_c1))
 
-attr_list_c2 = class_2_df[attribute].to_list()
-print(np.var(attr_list_c2))
+# attr_list_c2 = class_2_df[attribute].to_list()
+# print(np.var(attr_list_c2))
 
-shapiro_attr_list_c1 = stats.shapiro(attr_list_c1) # rozkład  jest normany
-shapiro_attr_list_c2 = stats.shapiro(attr_list_c2) # rozkład  jest normany
-# shapiro_age_c3 = stats.shapiro(age_c3) # rozkład nie jest normany
+# shapiro_attr_list_c1 = stats.shapiro(attr_list_c1) # rozkład  jest normany
+# shapiro_attr_list_c2 = stats.shapiro(attr_list_c2) # rozkład  jest normany
+# # shapiro_age_c3 = stats.shapiro(age_c3) # rozkład nie jest normany
 
-print(shapiro_attr_list_c1)
-print(shapiro_attr_list_c2)
+# print(shapiro_attr_list_c1)
+# print(shapiro_attr_list_c2)
 
-print(stats.ttest_ind(shapiro_attr_list_c1, shapiro_attr_list_c2, equal_var=True))
+# print(stats.ttest_ind(shapiro_attr_list_c1, shapiro_attr_list_c2, equal_var=True))
 
 
 # print(shapiro_age_c3)
